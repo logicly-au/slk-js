df966bb073cdfc332d87db08dbbc30181a0b0079
David Bindloss <davidb@strategicdata.com.au>
2019-02-11 07:45:43 +1100

    initial commit

---
+++ slk.test.js
@@ -0,0 +1,98 @@
+const SLK = require("./dist/slk.min.js");
+
+const fixtures = [
+  {
+    slk: "TEEOH070619541",
+    crockford: "82CZ43SSMG3AB42HFC7TVXKBD35N4F7C",
+    hex: "4099f20f39a406a590517b0fadf66b68cb523cec"
+  },
+  {
+    slk: "TEEOH070619542",
+    crockford: "5PJKG2HX9K97ETGM15B6J1T2R1VKJYCD",
+    hex: "2da5380a3d4cd2776a140956690742c07739798d"
+  },
+  {
+    slk: "O22OH070619543",
+    crockford: "CQ00PF0BHMMHZA47VXSKCD6A3E5BYWXW",
+    hex: "65c00b3c0b8d291fa887df733634ca1b8abf73bc"
+  },
+  {
+    slk: "TEEO2070619549",
+    crockford: "24S47HEWM6XK02R2XWSJM9WT6ND6BN6P",
+    hex: "113243c5dca1bb300b02ef332a279a355a65d4d6"
+  },
+  {
+    slk: "999OH070619541",
+    crockford: "45B4VDJ3RX43A05ASGG26PMVGWBJGRXY",
+    hex: "21564db643c7483500aacc20235a9b87172863be"
+  },
+  {
+    slk: "TEE99070619542",
+    crockford: "RQPY9ATA9KDQ5E51S9QBF96RVTAGZGX9",
+    hex: "c5ede4ab4a4cdb72b8a1ca6eb7a4d8de950fc3a9"
+  },
+  {
+    slk: "99999070619543",
+    crockford: "M0DZ0V4F4KDTG0F991NT4ZDQW9C24D02",
+    hex: "a01bf06c8f24dba801e9486ba27db7e258223402"
+  },
+  {
+    slk: "TEEOH070619549",
+    crockford: "75VMHRKX96JZDW7X4JEW8WZF9ZZDP1TM",
+    hex: "397748e27d49a5f6f0fd249dc473ef4ffedb0754"
+  },
+  {
+    slk: "TEEOH070619541",
+    crockford: "82CZ43SSMG3AB42HFC7TVXKBD35N4F7C",
+    hex: "4099f20f39a406a590517b0fadf66b68cb523cec"
+  },
+  {
+    slk: "TEEOH090999991",
+    crockford: "CER7VTWBKBSVFAX51WA1YX4266S99ZD4",
+    hex: "63b07deb8b9af3b7aba50f141f748231b294fda4"
+  },
+  {
+    slk: "MEIOH070619543",
+    crockford: "AGYE3M2RGDRCEZPS5Z9EA9M5JQ5MY54W",
+    hex: "543ce1d0588370c77ed92fd2e5268595cb4f149c"
+  },
+  {
+    slk: "NN2NI131220002",
+    crockford: "VK6XCFWS89KE5AJ66HY7Z195P8KNVFKM",
+    hex: "dccdd63f994266e2aa46347c7f8525b2275dbe74"
+  }
+];
+
+let invalid = [
+  '33333010120013',
+  'AAA99010120016',
+  'AA999010120012',
+  '99999010119993x',
+  '9999901011999',
+  '99999999999999'
+];
+
+test("slk is what it is expected to be", () => {
+  var x = SLK.generate("David", "Bindloss", "1989-01-23", "1");
+  expect(x.slk).toBe("INLAV230119891");
+});
+
+fixtures.map((x) => {
+  test(`${x.slk} slk is valid`, () => {
+    expect(SLK.is_valid(x.slk).slk).toBe(true);
+    expect(SLK.is_valid(x.slk).date).toBe(true);
+  });
+  test(`${x.crockford} crockford is valid`, () => {
+    expect(SLK.is_valid(x.crockford).crockford).toBe(true);
+  });
+  test(`${x.hex} hex is valid`, () => {
+    expect(SLK.is_valid(x.hex).hex).toBe(true);
+  });
+});
+
+invalid.map((x) => {
+  test(`${x} is an invalid value`, () => {
+    let n = Object.values(SLK.is_valid(x)).some(x => x === true);
+    expect(n).toBe(false);
+  });
+})
---
+++ src/index.js
@@ -0,0 +1,95 @@
+import unorm from "unorm";
+import Nibbler from "nibbler-crockford";
+import Latin1 from "crypto-js/enc-latin1";
+import SHA1 from "crypto-js/sha1";
+
+const SLK_REGEX = /^([A-Z]{3}|[A-Z]{2}2|[A-Z]22|222|999)([A-Z]{2}|[A-Z]2|22|99)([0-9]{8})([1-3]|9)$/;
+const HEX_REGEX = /^[0-9A-Fa-f]{40}$/;
+const CROCKFORD_REGEX = /^[0-9ABCDEFGHJKMNPQRSTVWXYZ]{32}$/;
+const DOB_REGEX = /^([0-2][0-9]|(3)[0-1])(((0)[0-9])|((1)[0-2]))\d{4}$/;
+
+const nibbler = new Nibbler({
+  dataBits: 8,
+  codeBits: 5,
+  keyString: "0123456789ABCDEFGHJKMNPQRSTVWXYZ",
+});
+
+const MISSING_CHAR = "2";
+const EMPTY_STRING = "";
+const FIRST_NAME_INDICES = [1, 2];
+const LAST_NAME_INDICES = [1, 2, 4];
+
+const arrayToString = (x, y = EMPTY_STRING) => x.join(y)
+const stringToArray = (x, y = EMPTY_STRING) => x.split(y)
+
+// dobString is expected in iso format yyyy-mm-dd
+const generate = (first = "", last = "", dobString, genderId) => {
+    first = sliceString(first, FIRST_NAME_INDICES);
+    last = sliceString(last, LAST_NAME_INDICES);
+    let dobArray = dobString.split("-");
+    dobArray.reverse();
+    let dob = dobArray.join(EMPTY_STRING);
+    genderId = genderId == 0 && 9 || genderId;
+    let slk = [last, first, dob, genderId].join(EMPTY_STRING).toUpperCase();
+    let hash = nibbler.encode(SHA1(slk).toString(Latin1));
+    return {slk, hash};
+}
+
+/**
+ * Check if an SLK is valid, one of the formats:
+ * 14 char SLK
+ * 32 char crockford encoded string
+ * 40 character hex string
+ * The date value is also checked if the slk is a valid 14 char SLK.
+ * @param {String} $slk String to check for validity
+ */
+const is_valid = $slk => {
+  let validations = {
+    'slk': SLK_REGEX.test($slk),
+    'hex': HEX_REGEX.test($slk),
+    'crockford': CROCKFORD_REGEX.test($slk)
+  };
+
+  if(validations.slk === true) {
+    let date = $slk.slice(5, -1);
+    validations.date = DOB_REGEX.test(date);
+    if(!validations.date) {
+      validations.slk = false;
+    }
+  }
+  return validations;
+}
+
+/**
+ * Return indices of a given string
+ * @param {String} $string  The string that will be sliced
+ * @param {Number[]} $indices Array of indices to extract from the string.
+ */
+const sliceString = ($string, $indices) => {
+  let ret = stringToArray("2".repeat($indices.length));
+
+  if(!!$string.length === false) {
+    return arrayToString(ret);
+  }
+  // normalize the name first
+  $string = unorm.nfkd($string);
+  stringToArray($string).map((e, i) => {
+    if($indices.includes(i)) {
+      ret[ret.indexOf("2")] = e;
+    }
+  });
+  return arrayToString(ret);
+}
+
+const exports = {
+  generate,
+  is_valid
+}
+
+if(typeof window !== 'undefined') {
+  window.SLK = exports;
+}
+
+if(typeof module !== 'undefined' && module.exports) {
+  module.exports = exports;
+}
